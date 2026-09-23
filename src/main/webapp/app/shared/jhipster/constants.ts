286a9f171a57b7ace147736e233a75a41ec7a247
manunununun <j.moscoso@udea.edu.co>
2026-09-22 21:34:05 -0500

    Initial version of lab01fsi generated by generator-jhipster@9.2.0

---
+++ src/main/webapp/app/shared/jhipster/constants.ts
@@ -0,0 +1,10 @@
+export const MESSAGE_ALERT_HEADER_NAME = 'x-lab01fsiapp-alert';
+export const MESSAGE_ERROR_HEADER_NAME = 'x-lab01fsiapp-error';
+export const MESSAGE_PARAM_HEADER_NAME = 'x-lab01fsiapp-params';
+
+export const AUTHENTICATION_TOKEN_KEY = 'jhi-authenticationToken';
+
+export enum Authority {
+  ADMIN = 'ROLE_ADMIN',
+  USER = 'ROLE_USER',
+}
